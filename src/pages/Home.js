35a9a237fb1e36e426a9f854a3151a3ac7a3bffc
Carla Roos Morales <carlaroosmorales@MacBook-Pro-som-tillhor-Carla.local>
2023-03-24 20:26:09 +0100

    basic layout

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Home() {
+    return(     
+        <>
+
+        <div class="subtitle">
+        <h2>Hej! Jag heter Carla och är</h2>
+        </div>
+        <div class="title">
+            <h1>INTERAKTIONSDESIGNER</h1>
+        </div>
+
+        <div class="terms">
+            <ul>
+                <li>UX & UI DESIGNER</li>
+                <li>UX RESEARCHER</li>
+                <li>FRONT-END</li>
+            </ul>
+        </div>
+        
+        </>
+    );
+}
+
+export default Home;
